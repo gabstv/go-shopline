61b152fe3d64ccca981a113f167cfb7280114d25
Gabriel Ochsenhofer <gabs@gabs.tv>
2015-04-16 18:50:05 -0300

    1

--- webservice.go
+++ webservice.go
@@ -1,5 +1,9 @@
 package shopline
 
+import (
+	"time"
+)
+
 const (
 	URL_BOLETO   = "https://shopline.itau.com.br/shopline/Itaubloqueto.asp"
 	URL_CONSULTA = "https://shopline.itau.com.br/shopline/consulta.asp"
@@ -12,7 +16,39 @@ type Webservice struct {
 	ChaveItau string
 }
 
+type BoletoDef struct {
+	Valor           float64
+	Obs             string
+	NomeCliente     string
+	CodigoInscricao string
+	NumeroInscricao string
+	CEP             string
+	Endereco        string
+	Bairro          string
+	Cidade          string
+	Estado          string
+	Vencimento      time.Time
+}
+
 func New(codigo, chave string) *Webservice {
 	ws := &Webservice{codigo, chave, "SEGUNDA12345ITAU"}
 	return ws
+}
+
+func (ws *Webservice) NewBoleto(boleto BoletoDef) {
+
+}
+
+func rjust(in, fill string, length int) string {
+	for len(in) < length {
+		in = fill + in
+	}
+	return in
+}
+
+func ljust(in, fill string, length int) string {
+	for len(in) < length {
+		in = in + fill
+	}
+	return in
 }
